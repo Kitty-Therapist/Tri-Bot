9af1879418f64ec6cf315ffd6d2df835cd9a4eaa
Kitty-Therapist <31807465+Kitty-Therapist@users.noreply.github.com>
2018-10-11 15:30:48 -0400

    Stop the bot from erroring out with reactions.
    Applied a fix for the bug where the bot would error out when trying to submit anything - reactions was an issue.

--- cogs/Submissions.py
+++ cogs/Submissions.py
@@ -8,11 +8,11 @@ from utils import Configuration, Permission
 class Submissions:
     def __init__(self, bot):
         self.bot:commands.Bot = bot
-        self.upvote = utils.get(bot.emojis, id=499401182427611136)
 
     @commands.command()
     async def submit(self, ctx, *content):
         channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "SUBMISSION_CHANNEL"))
+        upvote = utils.get(self.bot.emojis, id=499401182427611136)
         if not channel:
             return await ctx.send("The submission channel is not configured, please tell a moderator.")
 
@@ -25,7 +25,7 @@ class Submissions:
                 f"**Artist:** {ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})\n"
                 f"**Link{'s' if len(links) > 1 else ''}:** {' '.join(links)}"
             ))
-            await message.add_reaction(self.upvote)
+            await message.add_reaction(upvote)
             return await ctx.send("I've sent your submission through, good luck with the event!")
         except discord.Forbidden:
             return await ctx.send("I can't send messages to the submission channel, please tell a moderator.")
