bc36f82de20f8add2601160ca6a402ead87dd6a7
GoldenPheonix <ihaxyou42@gmail.com>
2018-10-14 14:09:34 +0800

    Delete json file upon event end

--- cogs/EventControl.py
+++ cogs/EventControl.py
@@ -17,6 +17,9 @@ class EventControl:
     @commands.command(hidden=True)
     async def start(self, ctx: commands.Context):
         """Starts the event!"""
+        if os.path.exists(f'submissions/{ctx.guild.id}.json') is True:
+            return await ctx.send("There is a event already running! Use the event end command before starting a new one.")
+
         heads = [187606096418963456, 298618155281154058, 169197827002466304, 263495765270200320, 117101067136794628, 164475721173958657, 191793155685744640]
         channel = ctx.bot.get_channel(int(Configuration.getConfigVar(ctx.guild.id, "SUBMISSION_CHANNEL")))
         everyone = None
@@ -32,14 +35,19 @@ class EventControl:
             if role.id == channel.guild.id:
                 everyone = role
                 await channel.set_permissions(everyone, read_messages=True, send_messages=False, add_reactions=False)
-                await ctx.send("Event has started!")
+        await ctx.send("Event has started!")
 
     @commands.command(hidden=True)
     async def end(self, ctx:commands.Context):
         """Ends the event!"""
+        if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
+            return await ctx.send("There is currently no event running.")   
+
         heads = [187606096418963456, 298618155281154058, 169197827002466304, 263495765270200320, 117101067136794628, 164475721173958657, 191793155685744640]
         channel = ctx.bot.get_channel(int(Configuration.getConfigVar(ctx.guild.id, "SUBMISSION_CHANNEL")))
         everyone = None
+
+        os.remove(f'submissions/{ctx.guild.id}.json')
 
         if ctx.author.id not in heads:
             return
@@ -48,7 +56,7 @@ class EventControl:
             if role.id == channel.guild.id:
                 everyone = role
                 await channel.set_permissions(everyone, read_messages=False)
-                await ctx.send("Event has ended!")
+        await ctx.send("Event has ended!")
 
 def setup(bot):
     bot.add_cog(EventControl(bot))
