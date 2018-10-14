75d10f49cb97e4e0807d9814136a9d1c83403f90
Kitty-Therapist <31807465+Kitty-Therapist@users.noreply.github.com>
2018-10-13 23:46:37 -0400

    Ready!

--- cogs/Submissions.py
+++ cogs/Submissions.py
@@ -1,14 +1,20 @@
-import asyncio
 import discord
 import re
+import asyncio
+import os
+import json
 
 from discord.ext import commands
 from discord import utils
 from utils import Configuration, Permission
 
+
 class Submissions:
     def __init__(self, bot):
-        self.bot:commands.Bot = bot
+        self.bot: commands.Bot = bot
+
+    if not os.path.exists('submissions'):
+        os.makedirs('submissions')
 
     @commands.command()
     async def submit(self, ctx, *content):
@@ -18,21 +24,34 @@ class Submissions:
             return await ctx.send("The submission channel is not configured, please tell a moderator.")
 
         links = re.findall(r"https?://\S+\.\S+", ' '.join(content))
-        if not links or len(links) > 5:
-            return await ctx.send("Your submission must contain at least one link, and no more than five!")
+        if not links or len(links) > 1:
+            return await ctx.send("Your submission must contain at least one link, and no more than one!")
 
         try:
-            message = await channel.send((
-                f"**Artist:** {ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})\n"
-                f"**Link{'s' if len(links) > 1 else ''}:** {' '.join(links)}"
-            ))
-            await message.add_reaction(upvote)
-            reply = await ctx.send("I've sent your submission through, good luck with the event!")
-            await asyncio.sleep(10)
-            await reply.delete()
-            await ctx.message.delete()
+            with open(f'submissions/{ctx.guild.id}.json', 'r') as infile:
+                data = json.load(infile)
+            if data[str(ctx.author.id)] is not None:
+                reply = await ctx.send("You already submitted the following: " + data[str(ctx.author.id)]['SUBMISSION_LINK'])
+                await asyncio.sleep(10)
+                await reply.delete()
+                await ctx.message.delete()
+                return
+            else:
+                message = await channel.send((
+                    f"**Artist:** {ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})\n"
+                    f"**Link{'s' if len(links) > 1 else ''}:** {' '.join(links)}"
+                ))
+                await message.add_reaction(upvote)
+                data[str(ctx.author.id)] = {'SUBMISSION_LINK': ', '.join(links)}
+                reply = await ctx.send("I've sent your submission through, good luck with the event!")
+                await asyncio.sleep(10)
+                await reply.delete()
+                await ctx.message.delete()
+            with open(f'submissions/{ctx.guild.id}.json', 'w') as outfile:
+                json.dump(data, outfile, indent=4)
         except discord.Forbidden:
             return await ctx.send("I can't send messages to the submission channel, please tell a moderator.")
+
 
 def setup(bot):
     bot.add_cog(Submissions(bot))
