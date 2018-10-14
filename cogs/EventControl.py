5ae9e7b8e458129f2db50a6f3bd84bbbd24a7874
GoldenPheonix <ihaxyou42@gmail.com>
2018-10-14 14:46:17 +0800

    Revert "Cross Server event magic part 1"
    This reverts commit fbd1d0e491e470719c107d67a9de05a058aff5a8.

--- cogs/EventControl.py
+++ cogs/EventControl.py
@@ -17,57 +17,45 @@ class EventControl:
     @commands.command(hidden=True)
     async def start(self, ctx: commands.Context):
         """Starts the event!"""
+        if os.path.exists(f'submissions/{ctx.guild.id}.json') is True:
+            return await ctx.send("There is a event already running! Use the event end command before starting a new one.")
+
         heads = [187606096418963456, 298618155281154058, 169197827002466304, 263495765270200320, 117101067136794628, 164475721173958657, 191793155685744640]
+        channel = ctx.bot.get_channel(int(Configuration.getConfigVar(ctx.guild.id, "SUBMISSION_CHANNEL")))
         everyone = None
+
+        data = {}
+        with open(f'submissions/{ctx.guild.id}.json', 'w') as outfile:
+            json.dump(data, outfile, indent=4)
 
         if ctx.author.id not in heads:
             return
 
-        for server in bot.guilds:
-            channel = ctx.bot.get_channel(int(Configuration.getConfigVar(server.id, "SUBMISSION_CHANNEL")))
-            if channel is None:
-                return await ctx.send("One of the event guilds do not have a submission channel set or the submission channel was deleted!")
-        
-        if os.path.exists(f'submissions/currentevent.json') is True:
-            return await ctx.send("There is a event already running! Use the event end command before starting a new one.")
-
-        data = {}
-        with open(f'submissions/currentevent.json', 'w') as outfile:
-            json.dump(data, outfile, indent=4)
-
-        for server in bot.guilds:
-            channel = ctx.bot.get_channel(int(Configuration.getConfigVar(server.id, "SUBMISSION_CHANNEL")))
-            for role in channel.guild.roles:
-                if role.id == channel.guild.id:
-                    everyone = role
-                    await channel.set_permissions(everyone, read_messages=True, send_messages=False, add_reactions=False)
+        for role in channel.guild.roles:
+            if role.id == channel.guild.id:
+                everyone = role
+                await channel.set_permissions(everyone, read_messages=True, send_messages=False, add_reactions=False)
         await ctx.send("Event has started!")
 
     @commands.command(hidden=True)
     async def end(self, ctx:commands.Context):
         """Ends the event!"""
+        if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
+            return await ctx.send("There is currently no event running.")   
+
         heads = [187606096418963456, 298618155281154058, 169197827002466304, 263495765270200320, 117101067136794628, 164475721173958657, 191793155685744640]
+        channel = ctx.bot.get_channel(int(Configuration.getConfigVar(ctx.guild.id, "SUBMISSION_CHANNEL")))
         everyone = None
-  
+
+        os.remove(f'submissions/{ctx.guild.id}.json')
+
         if ctx.author.id not in heads:
             return
 
-        for server in bot.guilds:
-            channel = ctx.bot.get_channel(int(Configuration.getConfigVar(server.id, "SUBMISSION_CHANNEL")))
-            if channel is None:
-                return await ctx.send("One of the event guilds do not have a submission channel set or the submission channel was deleted!")
-
-        if os.path.exists(f'submissions/currentevent.json') is False:
-            return await ctx.send("There is currently no event running.")   
-
-        os.remove(f'submissions/currentevent.json')
-
-        for server in bot.guilds:
-            channel = ctx.bot.get_channel(int(Configuration.getConfigVar(server.id, "SUBMISSION_CHANNEL")))
-            for role in channel.guild.roles:
-                if role.id == channel.guild.id:
-                    everyone = role
-                    await channel.set_permissions(everyone, read_messages=False)
+        for role in channel.guild.roles:
+            if role.id == channel.guild.id:
+                everyone = role
+                await channel.set_permissions(everyone, read_messages=False)
         await ctx.send("Event has ended!")
 
 def setup(bot):
