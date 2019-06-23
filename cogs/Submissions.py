d98a393f486d5b85d6528c789b6906339ac69a0b
Ghoul <artmazingmao@gmail.com>
2019-06-23 14:02:16 -0400

    Add blacklist support

--- cogs/Submissions.py
+++ cogs/Submissions.py
@@ -7,7 +7,7 @@ import json
 from discord.ext import commands
 from discord.ext.commands import BucketType
 from discord import utils
-from utils import Configuration, Permission
+from utils import Configuration
 
 
 class Submissions(commands.Cog):
@@ -26,49 +26,15 @@ class Submissions(commands.Cog):
 
     @submit.command()
     @commands.cooldown(1, 10, BucketType.user)
-    async def art(self, ctx, *content):
-        channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "GENERAL_ART_CHANNEL"))
-
-        if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
-            data = {}
-            with open(f'submissions/{ctx.guild.id}.json', 'w') as outfile:
-                json.dump(data, outfile, indent=4)
-
-        if not channel:
-            return await ctx.send("The submission channel is not configured, please tell a moderator.")
-
-        links = re.findall(r"https?://\S+\.[^\s>]+", ' '.join(content))
-        if not links or len(links) > 1:
-            return await ctx.send("Your submission must contain at least one link, and no more than one!")
-
-        try:
-            with open(f'submissions/{ctx.guild.id}.json', 'r') as infile:
-                data = json.load(infile)
-            if str(ctx.author.id) in data:
-                reply = await ctx.send("You already submitted the following: " + data[str(ctx.author.id)]['SUBMISSION_LINK'])
-                await asyncio.sleep(10)
-                await reply.delete()
-                await ctx.message.delete()
-                return
-            else:
-                message = await channel.send((
-                    f"**Artist:** {ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})\n"
-                    f"**Link{'s' if len(links) > 1 else ''}:** {' '.join(links)}"
-                ))
-                data[str(ctx.author.id)] = {'SUBMISSION_LINK': ', '.join(links)}
-                reply = await ctx.send("I've sent your submission through, good luck with the event!")
-                await asyncio.sleep(10)
-                await reply.delete()
-                await ctx.message.delete()
-            with open(f'submissions/{ctx.guild.id}.json', 'w') as outfile:
-                json.dump(data, outfile, indent=4)
-        except discord.Forbidden:
-            return await ctx.send("I can't send messages to the submission channel, please tell a moderator.")
-
-    @submit.command()
-    @commands.cooldown(1, 10, BucketType.user)
-    async def fanart(self, ctx, *content):
+    async def fanart(self, ctx, *content : str):
+        type(content)
         channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "FAN_ART_CHANNEL"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
 
         if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
             data = {}
@@ -81,6 +47,7 @@ class Submissions(commands.Cog):
         links = re.findall(r"https?://\S+\.[^\s>]+", ' '.join(content))
         if not links or len(links) > 1:
             return await ctx.send("Your submission must contain at least one link, and no more than one!")
+            pass
 
         try:
             with open(f'submissions/{ctx.guild.id}.json', 'r') as infile:
@@ -110,6 +77,12 @@ class Submissions(commands.Cog):
     @commands.cooldown(1, 10, BucketType.user)
     async def emoji(self, ctx, *content):
         channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "EMOJIS_CHANNEL"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
 
         if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
             data = {}
@@ -151,6 +124,12 @@ class Submissions(commands.Cog):
     @commands.cooldown(1, 10, BucketType.user)
     async def merch(self, ctx, *content):
         channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "MERCH_CHANNEL"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
 
         if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
             data = {}
@@ -192,6 +171,12 @@ class Submissions(commands.Cog):
     @commands.cooldown(1, 10, BucketType.user)
     async def music(self, ctx, *content):
         channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "MUSIC_CHANNEL"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
 
         if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
             data = {}
@@ -233,6 +218,12 @@ class Submissions(commands.Cog):
     @commands.cooldown(1, 10, BucketType.user)
     async def story(self, ctx, *content):
         channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "STORY_CHANNEL"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
 
         if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
             data = {}
@@ -272,8 +263,14 @@ class Submissions(commands.Cog):
 
     @submit.command()
     @commands.cooldown(1, 10, BucketType.user)
-    async def bot(self, ctx, *content):
-        channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "GENERAL_BOT_CHANNEL"))
+    async def social(self, ctx, *content):
+        channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "SOCIAL"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
 
         if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
             data = {}
@@ -298,7 +295,7 @@ class Submissions(commands.Cog):
                 return
             else:
                 message = await channel.send((
-                    f"**Bot Developer:** {ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})\n"
+                    f"**Social Bot Developer:** {ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})\n"
                     f"**Link{'s' if len(links) > 1 else ''}:** {' '.join(links)}"
                 ))
                 data[str(ctx.author.id)] = {'SUBMISSION_LINK': ', '.join(links)}
@@ -315,6 +312,12 @@ class Submissions(commands.Cog):
     @commands.cooldown(1, 10, BucketType.user)
     async def functional(self, ctx, *content):
         channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "FUNCTIONAL"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
 
         if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
             data = {}
@@ -356,6 +359,12 @@ class Submissions(commands.Cog):
     @commands.cooldown(1, 10, BucketType.user)
     async def entertainment(self, ctx, *content):
         channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "ENTERTAINMENT"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
 
         if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
             data = {}
@@ -395,8 +404,61 @@ class Submissions(commands.Cog):
 
     @submit.command()
     @commands.cooldown(1, 10, BucketType.user)
+    async def moderation(self, ctx, *content):
+        channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "MODERATION"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
+
+        if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
+            data = {}
+            with open(f'submissions/{ctx.guild.id}.json', 'w') as outfile:
+                json.dump(data, outfile, indent=4)
+
+        if not channel:
+            return await ctx.send("The submission channel is not configured, please tell a moderator.")
+
+        links = re.findall(r"https?://\S+\.[^\s>]+", ' '.join(content))
+        if not links or len(links) > 1:
+            return await ctx.send("Your submission must contain at least one link, and no more than one!")
+
+        try:
+            with open(f'submissions/{ctx.guild.id}.json', 'r') as infile:
+                data = json.load(infile)
+            if str(ctx.author.id) in data:
+                reply = await ctx.send("You already submitted the following: " + data[str(ctx.author.id)]['SUBMISSION_LINK'])
+                await asyncio.sleep(10)
+                await reply.delete()
+                await ctx.message.delete()
+                return
+            else:
+                message = await channel.send((
+                    f"**Moderation Bot Developer:** {ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})\n"
+                    f"**Link{'s' if len(links) > 1 else ''}:** {' '.join(links)}"
+                ))
+                data[str(ctx.author.id)] = {'SUBMISSION_LINK': ', '.join(links)}
+                reply = await ctx.send("I've sent your submission through, good luck with the event!")
+                await asyncio.sleep(10)
+                await reply.delete()
+                await ctx.message.delete()
+            with open(f'submissions/{ctx.guild.id}.json', 'w') as outfile:
+                json.dump(data, outfile, indent=4)
+        except discord.Forbidden:
+            return await ctx.send("I can't send messages to the submission channel, please tell a moderator.")
+
+    @submit.command()
+    @commands.cooldown(1, 10, BucketType.user)
     async def shitpost(self, ctx, *content):
         channel = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "SHITPOST"))
+        censor = self.bot.get_channel(Configuration.getConfigVar(ctx.guild.id, "CENSORED_LOGS"))
+        blacklist = Configuration.getConfigVar(ctx.guild.id, "BAD_LINKS")
+
+        for badlink in blacklist:
+            if badlink in content[0]:
+                return await censor.send(f":warning: **{ctx.author.name}#{ctx.author.discriminator} (``{ctx.author.id}``) has attempted to submit a censored link. Please view the link at your own caution:**```{content}```")
 
         if os.path.exists(f'submissions/{ctx.guild.id}.json') is False:
             data = {}
